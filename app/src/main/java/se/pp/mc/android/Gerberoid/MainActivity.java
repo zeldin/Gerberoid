bad9ff6bdf4b5c6f8853ce0d4ce4380c5f1d02dd
Markus Fußenegger <fussenegger@mobile-software.ag>
2019-03-29 22:06:42 +0100

    up to date dependencies increased min version changed file loading removed read storage permission

--- app/src/main/java/se/pp/mc/android/Gerberoid/MainActivity.java
+++ app/src/main/java/se/pp/mc/android/Gerberoid/MainActivity.java
@@ -19,13 +19,11 @@
 
 package se.pp.mc.android.Gerberoid;
 
-import android.app.Activity;
 import android.app.AlertDialog;
 import android.content.ClipData;
 import android.content.Intent;
 import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
-import android.Manifest;
 import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
@@ -33,24 +31,25 @@ import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
-import android.view.MenuItem;
 import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.Spinner;
 import android.widget.Toast;
 
-import com.ipaulpro.afilechooser.utils.FileUtils;
-
+import java.io.BufferedInputStream;
 import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 
 
-public class MainActivity extends AppCompatActivity
-    implements Toolbar.OnMenuItemClickListener {
-
+public class MainActivity extends AppCompatActivity implements Toolbar.OnMenuItemClickListener {
 
     private static final int REQUEST_GERBER = 10001;
-    private static final int REQUEST_DRILL  = 10002;
+    private static final int REQUEST_DRILL = 10002;
 
     private GerberViewer gerber;
     private Layers layers;
@@ -64,234 +63,231 @@ public class MainActivity extends AppCompatActivity
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-	Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-	setSupportActionBar(toolbar);
-	toolbar = (Toolbar) findViewById(R.id.toolbar_bottom);
-	toolbar.setOnMenuItemClickListener(this);
-	toolbar.inflateMenu(R.menu.bottom_toolbar);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        toolbar = (Toolbar) findViewById(R.id.toolbar_bottom);
+        toolbar.setOnMenuItemClickListener(this);
+        toolbar.inflateMenu(R.menu.bottom_toolbar);
 
         gerber = (GerberViewer) findViewById(R.id.gerber_viewer);
         if (gerber != null) {
-	    gerber.onCreate();
-	    gerber.onRestoreInstanceState((savedInstanceState == null? new Bundle() : savedInstanceState));
-	    layers = gerber.getLayers();
-	    viewPort = gerber.getViewPort();
-	    displayOptions = gerber.getDisplayOptions();
-	} else {
-	    layers = null;
-	    viewPort = null;
-	    displayOptions = null;
-	}
-	layerSpinner = (Spinner) findViewById(R.id.layer_spinner);
-	if (layerSpinner != null) {
-	    layerSpinner.setAdapter(new LayerSpinnerAdapter(this, layers));
-	    layerSpinner.setSelection(layers.getActiveLayer());
-	    layerSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
-		    @Override
-		    public void onItemSelected(AdapterView<?> parent, View view,
-					       int pos, long id) {
-			layers.setActiveLayer(pos);
-		    }
+            gerber.onCreate();
+            gerber.onRestoreInstanceState((savedInstanceState == null ? new Bundle() : savedInstanceState));
+            layers = gerber.getLayers();
+            viewPort = gerber.getViewPort();
+            displayOptions = gerber.getDisplayOptions();
+        } else {
+            layers = null;
+            viewPort = null;
+            displayOptions = null;
+        }
+        layerSpinner = (Spinner) findViewById(R.id.layer_spinner);
+        if (layerSpinner != null) {
+            layerSpinner.setAdapter(new LayerSpinnerAdapter(this, layers));
+            layerSpinner.setSelection(layers.getActiveLayer());
+            layerSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+                @Override
+                public void onItemSelected(AdapterView<?> parent, View view,
+                                           int pos, long id) {
+                    layers.setActiveLayer(pos);
+                }
 
-		    @Override
-		    public void onNothingSelected(AdapterView<?> parent) {
-		    }
-		});
-	}
-	drawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
-	toolsDrawer = findViewById(R.id.tools_drawer);
-	if (toolsDrawer != null)
-	    new ToolsDrawer(this, toolsDrawer, displayOptions);
+                @Override
+                public void onNothingSelected(AdapterView<?> parent) {
+                }
+            });
+        }
+        drawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        toolsDrawer = findViewById(R.id.tools_drawer);
+        if (toolsDrawer != null)
+            new ToolsDrawer(this, toolsDrawer, displayOptions);
     }
 
     @Override
     protected void onSaveInstanceState(Bundle savedInstanceState) {
-	super.onSaveInstanceState(savedInstanceState);
-	if (gerber != null)
-	    gerber.onSaveInstanceState(savedInstanceState);
+        super.onSaveInstanceState(savedInstanceState);
+        if (gerber != null)
+            gerber.onSaveInstanceState(savedInstanceState);
     }
 
     @Override
-    protected void onDestroy ()
-    {
-	drawerLayout = null;
-	toolsDrawer = null;
-	layerSpinner = null;
-	layers = null;
-	viewPort = null;
-	displayOptions = null;
-	if (gerber != null) {
-	    gerber.onDestroy();
-	    gerber = null;
-	}
-	super.onDestroy();
+    protected void onDestroy() {
+        drawerLayout = null;
+        toolsDrawer = null;
+        layerSpinner = null;
+        layers = null;
+        viewPort = null;
+        displayOptions = null;
+        if (gerber != null) {
+            gerber.onDestroy();
+            gerber = null;
+        }
+        super.onDestroy();
     }
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu)
-    {
+    public boolean onCreateOptionsMenu(Menu menu) {
         MenuInflater inflater = getMenuInflater();
         inflater.inflate(R.menu.action_menu, menu);
         return true;
     }
 
     @Override
-    public boolean onOptionsItemSelected(MenuItem item)
-    {
-	switch (item.getItemId()) {
-	case R.id.action_settings:
-	    if (drawerLayout.isDrawerOpen(toolsDrawer))
-		drawerLayout.closeDrawer(toolsDrawer);
-	    else
-		drawerLayout.openDrawer(toolsDrawer);
-	    break;
-	case R.id.action_zoom_in:
-	    viewPort.SetPreviousZoom();
-	    break;
-	case R.id.action_zoom_out:
-	    viewPort.SetNextZoom();
-	    break;
-	case R.id.action_zoom_fit:
-	    viewPort.Zoom_Automatique();
-	    break;
-	case R.id.action_about:
-	    String version = "?";
-	    try {
-		PackageInfo pInfo = getPackageManager().getPackageInfo(getPackageName(), 0);
-		version = pInfo.versionName;
-	    } catch(PackageManager.NameNotFoundException e) {}
-	    AlertDialog.Builder builder = new AlertDialog.Builder(this);
-	    builder.setTitle(R.string.action_about)
-		.setMessage(getResources().getString(R.string.about_text, version))
-		.create()
-		.show();
-	    break;
-	}
-	return true;
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_settings:
+                if (drawerLayout.isDrawerOpen(toolsDrawer))
+                    drawerLayout.closeDrawer(toolsDrawer);
+                else
+                    drawerLayout.openDrawer(toolsDrawer);
+                break;
+            case R.id.action_zoom_in:
+                viewPort.SetPreviousZoom();
+                break;
+            case R.id.action_zoom_out:
+                viewPort.SetNextZoom();
+                break;
+            case R.id.action_zoom_fit:
+                viewPort.Zoom_Automatique();
+                break;
+            case R.id.action_about:
+                String version = "?";
+                try {
+                    PackageInfo pInfo = getPackageManager().getPackageInfo(getPackageName(), 0);
+                    version = pInfo.versionName;
+                } catch (PackageManager.NameNotFoundException e) {
+                }
+                AlertDialog.Builder builder = new AlertDialog.Builder(this);
+                builder.setTitle(R.string.action_about)
+                        .setMessage(getResources().getString(R.string.about_text, version))
+                        .create()
+                        .show();
+                break;
+        }
+        return true;
     }
 
     @Override
-    public boolean onMenuItemClick(MenuItem item)
-    {
-	switch (item.getItemId()) {
-	case R.id.action_gerber:
-	    SelectFile(REQUEST_GERBER);
-	    break;
-	case R.id.action_drill:
-	    SelectFile(REQUEST_DRILL);
-	    break;
-	case R.id.action_clear:
-	    layers.Clear_DrawLayers();
-	    layerSpinner.setSelection(layers.getActiveLayer());
-	    break;
-	}
-	return true;
+    public boolean onMenuItemClick(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_gerber:
+                SelectFile(REQUEST_GERBER);
+                break;
+            case R.id.action_drill:
+                SelectFile(REQUEST_DRILL);
+                break;
+            case R.id.action_clear:
+                layers.Clear_DrawLayers();
+                layerSpinner.setSelection(layers.getActiveLayer());
+                break;
+        }
+        return true;
+    }
+
+    private void SelectFile(int requestCode) {
+
+        final Intent intent = new Intent(Intent.ACTION_OPEN_DOCUMENT);
+        intent.addCategory(Intent.CATEGORY_OPENABLE);
+        intent.setType("*/*");
+        startActivityForResult(intent, requestCode);
+
+    }
+
+    private boolean LoadThing(int requestCode, Uri uri) {
+
+        final File file = toPrivateFile(uri);
+        if (file == null) {
+            return false;
+        }
+
+        boolean success;
+        if (requestCode == REQUEST_GERBER) {
+            success = layers.LoadGerber(file);
+        } else {
+            success = layers.LoadDrill(file);
+        }
+
+        file.delete();
+
+        return success;
+
     }
 
     @Override
-    public void onRequestPermissionsResult(int requestCode,
-					   String[] permissions,
-					   int[] grantResults)
-    {
-	int result = PackageManager.PERMISSION_DENIED;
-	for(int i=0; i<permissions.length; i++)
-	    if (Manifest.permission.READ_EXTERNAL_STORAGE.equals(permissions[i])) {
-		result = grantResults[i];
-		break;
-	    }
-	if (result == PackageManager.PERMISSION_GRANTED)
-	    SelectFile(requestCode);
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        switch (requestCode) {
+            case REQUEST_GERBER:
+            case REQUEST_DRILL:
+                if (resultCode == RESULT_OK) {
+                    ClipData clipData = null;
+                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN)
+                        clipData = data.getClipData();
+                    if (clipData != null) {
+                        final int n = clipData.getItemCount();
+                        for (int i = 0; i < n; i++) {
+                            final int oldActiveLayer = layers.getActiveLayer();
+                            if (!LoadThing(requestCode,
+                                    clipData.getItemAt(i).getUri()))
+                                break;
+                            if (layers.getActiveLayer() == oldActiveLayer &&
+                                    i + 1 < n) {
+
+                                Toast.makeText(this, R.string.no_more_empty_layers,
+                                        Toast.LENGTH_LONG).show();
+                                break;
+                            }
+                        }
+                    } else {
+                        LoadThing(requestCode, data.getData());
+                    }
+                    layerSpinner.setSelection(layers.getActiveLayer());
+                }
+                break;
+        }
     }
 
-    private boolean GetFilePermission(int requestCode)
-    {
-	if (Build.VERSION.SDK_INT < Build.VERSION_CODES.M)
-	    return true;
-
-	if (checkSelfPermission(Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED)
-	    return true;
-
-	requestPermissions(new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, requestCode);
-	return false;
+    Layers getLayers() {
+        return layers;
     }
 
-    private void SelectFile(int requestCode)
-    {
-	int titleResource;
-	switch(requestCode) {
-	case REQUEST_GERBER:
-	    titleResource = R.string.file_request_gerber;
-	    break;
-	case REQUEST_DRILL:
-	    titleResource = R.string.file_request_drill;
-	    break;
-	default:
-	    return;
-	}
-	if (!GetFilePermission(requestCode))
-	    return;
-	final Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
-	final String octetStream = "application/octet-stream";
-	intent.setType(octetStream);
-	if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT &&
-	    requestCode == REQUEST_GERBER) {
-	    String[] extraMimeTypes = {"application/vnd.gerber", octetStream};
-	    intent.putExtra(Intent.EXTRA_MIME_TYPES, extraMimeTypes);
-	}
-	intent.addCategory(Intent.CATEGORY_OPENABLE);
-	if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN_MR2)
-	    intent.putExtra(Intent.EXTRA_ALLOW_MULTIPLE, true);
-	startActivityForResult(Intent.createChooser(intent,
-						    getResources().getString(titleResource)),
-			       requestCode);
+    DisplayOptions getDisplayOptions() {
+        return displayOptions;
     }
 
-    private boolean LoadThing(int requestCode, Uri uri)
-    {
-	final File file = FileUtils.getFile(this, uri);
-	if (file == null)
-	    return false;
+    private File toPrivateFile(Uri uri) {
 
-	if (requestCode == REQUEST_GERBER)
-	    return layers.LoadGerber(file);
-	else
-	    return layers.LoadDrill(file);
+        try {
+
+            InputStream inputStream = new BufferedInputStream(getContentResolver().openInputStream(uri));
+            File tmp = File.createTempFile("gerber", null);
+            writeToFile(inputStream, tmp);
+            return tmp;
+
+        } catch (IOException e) {
+            return null;
+        }
+
     }
 
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data)
-    {
-	switch (requestCode) {
-	case REQUEST_GERBER:
-	case REQUEST_DRILL:
-	    if (resultCode == RESULT_OK) {
-		ClipData clipData = null;
-		if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN)
-		    clipData = data.getClipData();
-		if (clipData != null) {
-		    final int n = clipData.getItemCount();
-		    for (int i = 0; i < n; i++) {
-			final int oldActiveLayer = layers.getActiveLayer();
-			if (!LoadThing(requestCode,
-				       clipData.getItemAt(i).getUri()))
-			    break;
-			if (layers.getActiveLayer() == oldActiveLayer &&
-			    i+1 < n) {
+    private static void writeToFile(InputStream is, File file) throws IOException {
 
-			    Toast.makeText(this, R.string.no_more_empty_layers,
-					   Toast.LENGTH_LONG).show();
-			    break;
-			}
-		    }
-		} else {
-		    LoadThing(requestCode, data.getData());
-		}
-		layerSpinner.setSelection(layers.getActiveLayer());
-	    }
-	    break;
-	}
+        OutputStream outputStream = null;
+
+        try {
+            outputStream = new FileOutputStream(file);
+
+            int read = 0;
+            byte[] bytes = new byte[1024];
+            while ((read = is.read(bytes)) != -1) {
+                outputStream.write(bytes, 0, read);
+            }
+        } finally {
+            if (outputStream != null) {
+                outputStream.close();
+            }
+        }
     }
 
-    Layers getLayers() { return layers; }
-    DisplayOptions getDisplayOptions() { return displayOptions; }
 }
+
+
